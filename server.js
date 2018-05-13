02c8367220fa444cd089dae72e9c6f4f52365226
raulanatol <raul@natol.es>
2018-05-13 18:03:17 +0100

    Morgan

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const moviesRouter = require('./src/api/movies');
 
+app.use(morgan('combined'));
 app.use(express.json());
 app.use('/movies', moviesRouter);
 
