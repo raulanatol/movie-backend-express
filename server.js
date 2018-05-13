b47370eb9953f3342658e4064c19f82257ed2a35
raulanatol <raul@natol.es>
2018-05-13 18:43:32 +0100

    ErrorHandler

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const notifier = require('node-notifier');
 const session = require('express-session');
+const methodOverride = require('method-override');
 const app = express();
 
 const sessionOptions = {
@@ -8,9 +10,21 @@ const sessionOptions = {
 };
 const moviesRouter = require('./src/api/movies');
 
+function errorHandler(req, res) {
+  const title = `Error in ${req.method} ${req.url}`;
+  notifier.notify({ title: 'Error', message: title });
+  res.status(500).send('Algo se ha roto!');
+}
+
 app.use(morgan('combined'));
 app.use(session(sessionOptions));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(methodOverride());
+  app.use(errorHandler);
+}
+
 app.use('/movies', moviesRouter);
 
 app.get('/', (req, res) => {
