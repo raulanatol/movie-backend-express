3428054f98525326fc1a5dcdafd2e1124f223166
raulanatol <raul@natol.es>
2018-05-13 18:11:44 +0100

    express-session

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const session = require('express-session');
 const app = express();
 
+const sessionOptions = {
+  secret: '1234'
+};
 const moviesRouter = require('./src/api/movies');
 
 app.use(morgan('combined'));
+app.use(session(sessionOptions));
 app.use(express.json());
 app.use('/movies', moviesRouter);
 
 app.get('/', (req, res) => {
+  if (req.session.views) {
+    req.session.views++;
+  } else {
+    req.session.views = 1;
+    console.log('Welcome');
+  }
+  console.log('VIEWS', req.session.views);
   res.json({ message: 'Hello world' });
 });
 
